2f6ca68e7ea6b9ce119df2d29d38ccfc1088cdf4
emii <eizquierd@gmail.com>
2012-12-02 18:55:22 +0100

    added waitbar for thresholding

--- multithreshstack.m
+++ multithreshstack.m
@@ -2,13 +2,13 @@
 % This function will count the number of mRNAs for 100 thresholds 
 % from 0 up to the maximum intensity of the input image.
 
-function nout = multithreshstack(ims,threshold_num)
+function nout = multithreshstack(ims,threshold_num,h)
 
 % Number of thresholds to compute
 npoints = threshold_num;
 
-fprintf('Computing threshold (of %d):    1',npoints);
-
+sprintf('Computing threshold (of %d):    1',npoints);
+wb = waitbar(0,'Computing thresholds :    ');
 for i = 1:npoints
   % Apply threshold
   bwl = ims > i/npoints;
@@ -19,6 +19,8 @@ for i = 1:npoints
   % Save count into variable nout
   nout(i) = n;
 
-  fprintf('\b\b\b%3i',i);
+  sprintf('\b\b\b%3i',i);
+  waitbar(0.01*i,wb,['Computing threshold :    ' num2str(i)])
 end;
 fprintf('\n');
+close(wb)
